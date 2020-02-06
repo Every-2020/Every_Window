19bc1e71b444d88c7400ac2424aeeb1f628fc490
KyungHoon0126 <48943501+KyungHoon0126@users.noreply.github.com>
2020-02-06 22:36:43 +0900

    add : 직장인 회원가입 메소드 제작

--- Every/Every.Core.SignUp/Service/SignUpService.cs
+++ Every/Every.Core.SignUp/Service/SignUpService.cs
@@ -11,20 +11,23 @@ namespace Every.Core.SignUp.Service
 {
     public class SignUpService
     {
+        #region SettingHttpRequest
         private const string LOGIN_URL = "/auth/login";
         private const string MEMBER_URL = "members/";
         private const string LOGOUT_URL = MEMBER_URL + "logout";
         private const string TOKEN_REFRESH_URL = "/token/refresh";
+        #endregion
 
-        public readonly string SIGNUP_URL = "/auth/register/student"; // 학생용 회원가입
+        public readonly string SIGNUP_STUDENT_URL = "/auth/register/student"; // 학생용 회원가입
+        public readonly string SIGNUP_WORKER_URL = "/auth/register/worker"; // 직장인용 회원가입
 
         public NetworkManager networkManager = new NetworkManager();
 
         /// <summary>
-        /// 회원가입 메소드
+        /// 학생 회원가입 메소드
         /// </summary>
         /// <param name="email", 아이디(이메일 사용)></param>
-        /// <param name="pw", 비밀 번호></param>
+        /// <param name="pw", 비밀번호></param>
         /// <param name="name", 이름></param>
         /// <param name="phone", 전화번호("-" 입력해줘야 함.)></param>
         /// <param name="birth_year", 태어난 년도></param>
@@ -39,7 +42,31 @@ namespace Every.Core.SignUp.Service
             jObject["phone"] = phone;
             jObject["birth_year"] = birth_year;
             jObject["school_id"] = school_id;
-            return await networkManager.GetResponse<Nothing>(SIGNUP_URL, Method.POST, jObject.ToString());
+            return await networkManager.GetResponse<Nothing>(SIGNUP_WORKER_URL, Method.POST, jObject.ToString());
+        }
+
+        /// <summary>
+        /// 직장인용 회원가입 메소드
+        /// </summary>
+        /// <param name="email", 아이디(이메일 사용)></param>
+        /// <param name="pw", 비밀번호></param>
+        /// <param name="name", 이름></param>
+        /// <param name="phone", 전화번호("-" 입력해줘야 함.)></param>
+        /// <param name="birth_year", 태어난 년도></param>
+        /// <param name="work_place", 근무 직장></param>
+        /// <param name="work_category", 근무 직종></param>
+        /// <returns></returns>
+        public async Task<TResponse<Nothing>> Worker_SignUp(string email, string pw, string name, string phone, string birth_year, string work_place, string work_category)
+        {
+            JObject jObject = new JObject();
+            jObject["email"] = email;
+            jObject["pw"] = pw;
+            jObject["name"] = name;
+            jObject["phone"] = phone;
+            jObject["birth_year"] = birth_year;
+            jObject["work_place"] = work_place;
+            jObject["work_category"] = work_category;
+            return await networkManager.GetResponse<Nothing>(SIGNUP_WORKER_URL, Method.POST, jObject.ToString());
         }
 
         public void SettingHttpRequest(string serverUrl)
