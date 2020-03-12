fbe1f1d4aca099963a1346ed327d9c163aa563ea
KyungHoon0126 <48943501+KyungHoon0126@users.noreply.github.com>
2020-03-12 21:49:20 +0900

    [ADD] : SplashScreen  추가

---
+++ Every/Every/View/SplashScreen.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Every_AdminWin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Every.View
+{
+    /// <summary>
+    /// Interaction logic for SplashScreen.xaml
+    /// </summary>
+    public partial class SplashScreen : Window
+    {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+            Loaded += SplashScreen_Loaded;
+        }
+
+        private void SplashScreen_Loaded(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
+            dispatcherTimer.Start();
+        }
+
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            dispatcherTimer.Stop();
+            this.Close();
+        }
+    }
+}
