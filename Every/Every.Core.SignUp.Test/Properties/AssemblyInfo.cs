dc812fb8ed0ba5531ead7b03c9045a6b79a48c7c
KyungHoon <kidi052812@gmail.com>
2020-06-22 10:20:31 +0900

    [ADD] : SignUp TDD

---
+++ Every/Every.Core.SignUp.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Every.Core.SignUp.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Every.Core.SignUp.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f760891b-b1b7-44b3-96bb-cd87a616234e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Every/Every.Core.SignUp.Test/SignUpTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Every.Core.SignUp.Test
+{
+    [TestClass]
+    public class SignUpTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
